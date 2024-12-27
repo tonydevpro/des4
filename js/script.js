bdfe862f929ff317581bc206da5bcfa0eb466a2f
tonydevpro <antonio.1358@hotmail.com>
2024-12-27 10:50:22 -0300

    aprimoramento

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
 
     let exibir = document.getElementById('numberList');
-    let resultado = [];
+    let resultadoImpar = [];
+    let resultadoPar = [];
     function imparOuPar (parametro){
         let resto = parametro % 2 ;
         if (resto == 0){
-            resultado.push(parametro)
+            resultadoPar.push(parametro)
         }
         else{
-            return
+            resultadoImpar.push(parametro)
         }
 }
    
-    document.querySelector('button').addEventListener('click', ()=>{
-    resultado =[''];
+    document.querySelector('#filterButtonPares').addEventListener('click', ()=>{
+    resultadoPar =[];
     let texto = document.querySelector('input').value;
     texto = texto.trim();
     if(texto.includes(' ')){
@@ -30,7 +31,31 @@
         imparOuPar(numero);
     }
         
-    exibir.innerHTML=resultado.join(',')
+    exibir.innerHTML=resultadoPar.join(',')
+    
+}
+})
+
+document.querySelector('#filterButtonImpares').addEventListener('click', ()=>{
+    resultadoImpar =[];
+    let texto = document.querySelector('input').value;
+    texto = texto.trim();
+    if(texto.includes(' ')){
+        alert('invalido! não pode conter espaços.')
+        return
+    }
+    else{
+    let partes = texto.split(',');
+    for (let index = 0; index < partes.length; index++) {
+        let numero = parseInt(partes[index], 10); // Converte para número
+        if (isNaN(numero)) {
+            alert(`${partes[index]} não é um número válido.`);
+            return;
+        }
+        imparOuPar(numero);
+    }
+        
+    exibir.innerHTML=resultadoImpar.join(',')
     
 }
 })
